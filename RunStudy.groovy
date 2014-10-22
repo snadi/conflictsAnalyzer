d6189de43c4800d14490e2f686fd1d105ab91743
Paola Accioly <prga@cin.ufpe.br>
2014-10-22 15:55:20 -0300

    adding class that integrates the different setps of th study

---
+++ RunStudy.groovy
@@ -0,0 +1,12 @@
+package conflictsAnalyzer
+
+class RunStudy {
+	//this class is supposed to integrate all the 4 steps involved to run the study
+	
+	
+	
+	public static void main (String[] args){
+		RunStudy study = new RunStudy()
+	}
+
+}
