c4c27bff51c44b4d79992aa60e83e1b3eb9f77a7
Paola Accioly <prga@cin.ufpe.br>
2014-10-29 14:57:22 -0300

    initializing integration

--- src/RunStudy.groovy
+++ src/RunStudy.groovy
@@ -1,31 +1,61 @@
+import edu.unl.cse.git.App;
+import net.wagstrom.research.github.Github;
+
 
 
 class RunStudy {
 	//this class is supposed to integrate all the 4 steps involved to run the study
+	
+	private String configurationProperties = 'configuration.properties'
 	public Study(){}
 	
-	public void run(String[] args){
+	public void run(String projectData){
 		
-		def projectsList = new File(args[0])
+		def projectsList = new File(projectData)
 		projectsList.eachLine {
 			runGitMiner(it)
-			runGremlinQuery()
+			/*runGremlinQuery()
 			runSedCommands()
-			runConflictsAnalyzer()
+			runConflictsAnalyzer()*/
 		}
 		
 		
 	}
 	
 	public void runGitMiner(String project){
-		String[] projectData = project.split(',')
-		String projectName = projectData[0].trim()
-		String projectRepo = projectData[1].trim()
-		updateConfigurationFile()
+		String[] projectData = project.split('/')
+		String projectName = projectData[1].trim()
+		String projectRepo = project
+		updateConfigurationFile(projectRepo)
+		runGithub()
+		runApp()
 	}
 	
-	private void updateConfigurationFile(){
+	private void runGithub(){
+		Github g = new Github()
+		String[] files = ['-c', this.configurationProperties]
+		g.run(files)
+	}
+	
+	private void runApp(){
+		App a = new App()
+		String[] files = ["-c", this.configurationProperties]
+		a.run(files)
+	}
+	
+	private void updateConfigurationFile(String projectRepo){
 		
+		def myFile = new File(this.configurationProperties)
+		def fileText = myFile.text
+		String replace1 = 'projects=' + projectRepo
+		String replace2 = 'git.repositories=' + projectRepo
+		def regex1 = /projects=.*/
+		def regex2 = /git.repositories=.*/
+	
+		fileText = fileText.replaceAll(regex1, replace1)
+		fileText = fileText.replaceAll(regex2, replace2)
+		
+		myFile.write(fileText)
 	}
 	
 	private void runGithubJava(){}
@@ -50,7 +80,7 @@ class RunStudy {
 	
 	public static void main (String[] args){
 		RunStudy study = new RunStudy()
-		study.run(args)
+		study.run('projects.data')
 	}
 
 }
