113a0e50659cef6e23a213f5a430621cc9f72df2
Paola Accioly <prga@cin.ufpe.br>
2014-12-10 15:39:10 -0300

    improving r script r to add columns to conflict rate table and bar chart to conflict patterns occurrences

--- resultsScript.r
+++ resultsScript.r
@@ -1,22 +1,57 @@
 #to do list:
 #place new column with conflict rate percentage
 
-importPath = "/Users/paolaaccioly/Documents/Doutorado/conflictsStudy/ConflictsAnalyzer/"
-exportPath = "/Users/paolaaccioly/Dropbox/Public/conflictpattern/conflictResults.html"
-
-conflictRateFile="projectsData.csv"
-#conflictPatternFile=""
-
-conflictRate = read.table(file=paste(importPath, conflictRateFile, sep=""), header=T)
-attach(conflictRate)
-
-#HTML file
-htmlFile = paste(exportPath, sep="")
-
-if (file.exists(htmlFile)) {
+deleteAllFiles <- function(exportPath, htmlFile) {
+  if (file.exists(htmlFile)) {
     file.remove(htmlFile)
   }
+  
+  fileToRemove = paste(exportPath, "BarChart.png", sep="")
+    if (file.exists(fileToRemove)) {
+      file.remove(fileToRemove)
+    }
+}
 
+
+main<-function(){
+importPath = "/Users/paolaaccioly/Documents/Doutorado/conflictsStudy/ConflictsAnalyzer/"
+exportPath = "/Users/paolaaccioly/Dropbox/Public/conflictpattern/"
+
+conflictRateFile="projectsData.csv"
+conflictPatternFile="patternsData.csv"
+
+#HTML file
+htmlFile = paste(exportPath, "conflictResults.html", sep="")
+
+#delete previous files
+deleteAllFiles(exportPath, htmlFile)
+
+#read and edit conflict rate table
+conflictRateTemp = read.table(file=paste(importPath, conflictRateFile, sep=""), header=T)
+
+sumMergeScenarios = sum(conflictRateTemp$Merge_Scenarios)
+sumConflictionScenarios = sum(conflictRateTemp$Conflicting_Scenarios)
+total = data.frame(Project="TOTAL", Merge_Scenarios=sumMergeScenarios, Conflicting_Scenarios=sumConflictionScenarios)
+conflictRate = rbind(conflictRateTemp, total)
+
+conflictRate["Conflict_Rate(%)"] <- (conflictRate$Conflicting_Scenarios/conflictRate$Merge_Scenarios)*100
+attach(conflictRate)
+
+#read and edit patterns pie chart
+conflictPatterns= read.table(file=paste(importPath, conflictPatternFile, sep=""), header=T)
+
+# bar chart 
+library(ggplot2)
+barChartFileName = paste("BarChart.png")
+png(paste(exportPath, barChartFileName, sep=""))
+
+df <- data.frame(patternName = factor(conflictPatterns$Pattern), occurrences = c(conflictPatterns$Occurrences))
+
+immigration_theme <- theme_update(axis.text.x = element_text(angle = 45, hjust = 0.50))
+p <- ggplot(data=df, aes(x= patternName, y= occurrences)) + geom_bar(stat="identity")
+print(p)
+p
+dev.off
 
 #HTML code
 library(R2HTML)
@@ -28,3 +63,9 @@ HTML.title(title, file=htmlFile, append=TRUE)
 HTML("<hr><h2>Conflict Rate</h2>", file=htmlFile, append=TRUE)
 HTML(conflictRate, file=htmlFile, append=TRUE)
 
+HTML("<hr><h2>Graphics - Conflict Patterns Occurrences</h2>", file=htmlFile, append=TRUE)
+HTMLInsertGraph(file=htmlFile, GraphFileName=barChartFileName, Align="center", append=TRUE)
+
+}
+
+main()
